0d2984af0bb6c5ffc6e950e6aeb659b5af744121
aynerd <olawaleayowande@gmail.com>
2023-03-16 14:56:08 +0100

    Added Car model

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Ground } from "./Ground";
+import "./style.css";
+import { CubeCamera,  Environment, OrbitControls,PerspectiveCamera} from "@react-three/drei"
+import { Car } from "./Car";
+import { Rings } from "./Rings";
+
+function CarShow() {
+  return (
+
+    <>
+    
+    <OrbitControls 
+        target={[0, 0.35, 0]}
+        maxPolarAngle={1.45}
+      />
+
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+      <color args={[0, 0, 0]} attach="background" />
+     
+      <CubeCamera resolution={256} frames={Infinity}>
+        {(texture) => (
+          <>
+            <Environment map={texture} />
+            <Car />
+          </>
+        )}
+      </CubeCamera>
+
+
+
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+       <Ground />
+       <Rings />
+
+    </>
+  )
+}
+
+
+function App() {
+  return (
+    <Suspense fallback={null}>
+    <Canvas shadows>
+      <CarShow />
+    </Canvas>
+  </Suspense>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
